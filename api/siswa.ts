bec38463ea88e6ab1dde677b8d2894f5374238a8
rasya19 <ismantoj88@gmail.com>
2026-05-17 05:23:22 +0700

    Update siswa.ts

--- api/siswa.ts
+++ api/siswa.ts
@@ -1,3 +1,3 @@
-export default async function handler(req: any, res: any) {
-  return res.status(200).json({ test: "API siswa jalan" })
+export default async function handler(req, res) {
+  return res.status(200).json({ test: "API siswa jalan", status: "ok" })
 }
